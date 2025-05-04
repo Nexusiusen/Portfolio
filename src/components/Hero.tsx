8e3446f7e18ae84aefc3689f28a3be53594810a0
Nexusiusen <Nexus.wikblad@gmail.com>
2025-05-04 22:24:03 +0200

    Hero page done!

---
+++ src/components/Hero.tsx
@@ -0,0 +1,15 @@
+const Hero = () => {
+  return (
+    <div className="flex justify-center p-12 py-92">
+      <div className="w-fit">
+        {/* Title */}
+        <h1 className="ApplyFontMont text2 text-9xl mb-10">
+          My <span className="font-bold text1">World</span>, My
+          <span className="font-bold text1">Rules</span>
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
